99e224972d27d252b16f6bad880c33da257ee544
rcbcastillo <rcbcastillo@gmail.com>
2023-05-04 15:18:57 +0100

    Add MovieSearch

---
+++ src/Components/EmployeeInfo.jsx
@@ -0,0 +1,15 @@
+import {useState, useEffect} from 'react';
+
+const EmployeeInfo = () => {
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    
+  });
+
+
+};
+
+export default EmployeeInfo;
